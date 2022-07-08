152690e08f25141ebb7128b19898d5fd30857d96
Nmsh <85759042+nm1-Dev@users.noreply.github.com>
2022-07-09 01:23:17 +0300

    Add Files

--- resources/nh-base/core/sv_core.lua
+++ resources/nh-base/core/sv_core.lua
@@ -1,24 +1,33 @@
 local function OnPlayerConnecting(playerName, setKickReason, deferrals)
     local src = source
     local identifiers = GetPlayerIdentifiers(src)
-    print('[nh-base] Player Connecting' .. playerName .. GetPlayerIdentifiers(src)[1])
+    print('[nh-base] Player Connecting :' .. playerName .. ' Info :' .. json.encode(GetPlayerIdentifiers(src)))
     deferrals.defer()
-    Wait(0)
+    Wait(100)
     deferrals.update(string.format("Hello %s. Your Steam ID is being checked.", playerName))
+    Wait(1000)
+    deferrals.update(string.format("Hello %s. Your Steam ID is being checked..", playerName))
+    Wait(500)
+    deferrals.update(string.format("Hello %s. Your Steam ID is being checked...", playerName))
     for _, v in pairs(identifiers) do
         if string.find(v, "steam") then
             steamIdentifier = v
             break
         end
     end
-    Wait(0)
+    Wait(1000)
     if not steamIdentifier then
-        deferrals.done('You Should open steam to connect to the server')
+        deferrals.done('❗You Should open Steam to connect to the server,join our discord for more information'.. nh.ServerDiscord)
     else
         deferrals.done()
     end
 end
 AddEventHandler("playerConnecting", OnPlayerConnecting)
+
+AddEventHandler('playerDropped', function (reason)
+    local src = source
+    print('Player ' .. GetPlayerName(source) ..' Player Info: ' ..json.encode(GetPlayerIdentifiers(src)).. ' dropped (Reason: ' .. reason .. ')')
+end)
 
 RegisterServerEvent('nh-base:server:Start')
 AddEventHandler('nh-base:server:Start', function()
